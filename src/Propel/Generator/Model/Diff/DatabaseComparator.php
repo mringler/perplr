765109f986017f0747db0add5cb45da19d5f70e6
Prateek Sureka <690278+surekap@users.noreply.github.com>
2026-07-29 19:22:42 +0530

    Fix excluded tables with schema-qualified names

--- src/Propel/Generator/Model/Diff/DatabaseComparator.php
+++ src/Propel/Generator/Model/Diff/DatabaseComparator.php
@@ -303,14 +303,18 @@ class DatabaseComparator
      */
     protected function isTableExcluded(Table $table): bool
     {
-        $tableName = $table->getName();
-        if (in_array($tableName, $this->excludedTables, true)) {
-            return true;
-        }
+        $tableNames = [$table->getName(), $table->getCommonName()];
 
-        foreach ($this->excludedTables as $excludedTableName) {
-            if (preg_match('/^' . str_replace('*', '.*', $excludedTableName) . '$/', $tableName)) {
+        foreach ($tableNames as $tableName) {
+            if (in_array($tableName, $this->excludedTables, true)) {
                 return true;
+            }
+
+            foreach ($this->excludedTables as $excludedTableName) {
+                $pattern = str_replace('\\*', '.*', preg_quote($excludedTableName, '/'));
+                if (preg_match('/^' . $pattern . '$/', $tableName)) {
+                    return true;
+                }
             }
         }
 
